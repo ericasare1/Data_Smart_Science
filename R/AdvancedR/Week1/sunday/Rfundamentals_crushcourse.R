fce51dfacf8982f2196d2ddb51df18101929a8f2
ericasa1 <eric.asare.kojo@gmail.com>
2022-02-06 15:58:51 -0500

    pushing all advancedR materials to github

---
+++ R/AdvancedR/Week1/sunday/Rfundamentals_crushcourse.R
@@ -0,0 +1,14 @@
+
+#___________________Title: Total Revenue Calculation ________________________________
+# Total revenue = price * quantity sold: this is also called total revenue formula or rule to calculate total revenue
+
+# learn how to create variables using
+#a. lets create price variable
+
+Price_corn <- 5
+Price_corn 
+
+#b lets create quantity sold variable
+
+quantity_sold_corn <- 20
+quantity_sold_corn
