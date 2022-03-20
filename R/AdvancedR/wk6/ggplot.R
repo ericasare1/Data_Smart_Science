350a298d091bd5a0176f21cdb3870dea75c1ec1b
ericasare1 <eric.asare.kojo@gmail.com>
2022-03-20 16:34:18 -0400

    outliers+missing info

--- R/AdvancedR/wk6/ggplot.R
+++ R/AdvancedR/wk6/ggplot.R
@@ -10,32 +10,35 @@ str(mtcars)
 
 #A.  line graphic
 
-ggplot(data = mtcars, 
-       aes(x = hp, y = cyl)) +
+p_a <- ggplot(data = mtcars, 
+       aes(x = hp, y = cyl)) + 
   geom_line()
 
+p_a
+
 #lets modity change color of line
-ggplot(data = mtcars, 
-       aes(x = hp, y = cyl, )
+mtcars %>%
+  ggplot(
+       aes(x = hp, y = cyl)
        ) +
-  geom_line(size = 2, color = "black")
+  geom_line(size = 0.2, color = "red")
 
 #lets add labels and change overall apperance of plot
 ggplot(data = mtcars, 
        aes(x = hp, y = cyl, )
 ) +
-  geom_line(size = 2, color = "black") +
-  labs(x = "number of cylinder", y = "horse power", title = "a line plot") + 
+  geom_line(size = 0.2, color = "black") +
+  labs(x = "horse power", y = "number of cylinders", title = "a line plot") + 
   theme_bw()
 
 #lets change the size of label texts and even its color
 ggplot(data = mtcars, 
-       aes(x = hp, y = cyl, )
+       aes(x = hp, y = cyl)
 ) +
-  geom_line(size = 2, color = "black") +
+  geom_line(size = 0.2, color = "black") +
   labs(x = "number of cylinder", y = "horse power", title = "a line plot") + 
   theme_bw() + 
-  theme(text = element_text(size=14, colour="dark green"))
+  theme(text = element_text(size=20, colour="dark green"))
 
 #rotate axis
 
@@ -48,13 +51,14 @@ p <- ggplot(data = mtcars,
   theme(text = element_text(size=14, colour="dark green"))
 
 p1 <- p + coord_flip()
+p1
 
 # save the plot
 ggsave("")
 
 #zoom in to aan area
 
-p + coord_cartesian(xlim = c(150, 260))
+p + coord_cartesian(xlim = c(0, 150))
 
 #++++++++++++++++++++++++++++++++++++++ Awesomeness Bonus__________make it all interactive
 #need to instal plotly
@@ -66,8 +70,8 @@ ggplotly(p + coord_cartesian(xlim = c(150, 260)))
 # set multiple plots into one figure
 library(ggpubr)
 
-figure <- ggarrange(p, p1,  bx, bx1,
-                    #labels = c("A", "B", "C", "D"),
+ggarrange(p, p1,  bx, bx1,
+                  #labels = c("line plot1", "B", "C", "box plot"),
                     ncol = 2, nrow = 2)
 
   
@@ -84,9 +88,9 @@ ggplot(data = mtcars,
 #C.  Box plot
 #. one factor column
 ggplot(data = mtcars, 
-       aes(x = factor(0), y = cyl )
+       aes(x = factor(0), y = cyl)
 ) +
-  geom_boxplot()
+  geom_boxplot() +labs(x = "", y = "number of cylinders", title = "the same information above applies")
 
 #. multiple factor column
 
@@ -105,12 +109,106 @@ bx1 <- ggplot(data = mtcars,
 
 # Use custom color palettes
 bx1 + scale_color_manual(values=c("#999999", "#E69F00", "#56B4E9"))
+bx1 + scale_color_manual(values=c("red", "green", "black"))
+
+#change the order of the discrete plots
+bx1 + scale_x_discrete(limits=c("8", "6", "4"))
 
 #managing legends
 bx1 + theme(legend.position="top")  #move legends to the top of the plot
 bx1 + theme(legend.position="bottom")  #move legends to the bottom of the plot
 bx1 + theme(legend.position="none") #No legend
 
-#change the order of the discrete plots
-bx1 + scale_x_discrete(limits=c("8", "6", "4"))
 
+# bar chart
+df <- data.frame(
+  
+  gdp_capita = rnorm(1000, 200, 54000)
+) %>% 
+  mutate(income_group = case_when(
+    gdp_capita < 1000 ~ "low_income",
+    gdp_capita > 1000 & gdp_capita < 12000 ~ "mid_income",
+    gdp_capita >12000 ~ "high_income"
+  )
+         
+)
+
+view(df)
+
+
+#bar chart
+a <- df %>%
+  group_by(income_group) %>%
+  summarize(num_countries = n()) 
+
+bar1 <- a %>%
+  ggplot(aes(x = income_group, y = num_countries)) + geom_col()
+
+
+bar2 <- df %>%
+  ggplot(aes(income_group)) + geom_bar()
+
+
+#one plot on top of each other
+
+bar3 <- a %>%
+  ggplot(aes(x = income_group, y = num_countries)) + geom_col() +
+  geom_point(aes(x = income_group, y = num_countries)) +
+  theme_bw()
+
+
+#detecting outliers
+#method1
+Q <- quantile(df$gdp_capita, probs=c(.25, .75), na.rm = FALSE)
+inter_qr <- IQR(Q)
+
+up_cutoff_outliers <-  Q[2]+1.5*inter_qr # Upper Range  
+lower_cutoff_outliers <-  Q[2]-1.5*inter_qr # Upper Range 
+
+#method 2
+outlier2 <- df %>% 
+  ggplot(aes(x = "", y = gdp_capita)) +
+  geom_boxplot(outlier.colour="red", outlier.shape=8,
+               outlier.size=4)
+
+#method 3
+boxplot(df$gdp_capita)$out
+df_out <- boxplot(df$gdp_capita, plot = FALSE)$out
+
+#__________________Missing information
+
+df2 <- df %>%
+  mutate(gdp_capita_some_na = as.numeric(ifelse(gdp_capita >50000 & gdp_capita  < 53000,"", gdp_capita)))
+view(df2)
+str(df2)
+#check which columns have missing info
+colSums(is.na(df2))
+
+#visualize missing info
+install.packages("VIM")              # Install VIM package
+library("VIM")                       # Load VIM
+
+aggr(df2) #$gdp_capita_some_na, combined = TRUE)                           # Create aggregation plot
+summary(aggr(df2, plot=FALSE))
+summary(aggr(df2, plot=TRUE))
+
+#https://www.rdocumentation.org/packages/VIM/versions/6.1.1/topics/barMiss
+barMiss(df2,interactive=TRUE)#$gdp_capita_some_na)                           # Create aggregation plot
+barMiss(df2$gdp_capita_some_na, only.miss = TRUE)
+histMiss(df2$gdp_capita_some_na)                           # Create aggregation plot
+
+
+
+#some inputations
+#. median and median
+median_imputation <- df2 %>%
+  mutate(gdp_capita_some_na = replace(gdp_capita_some_na,
+                                  is.na(gdp_capita_some_na),
+                                  median(gdp_capita_some_na, na.rm = TRUE)))
+
+mean_imputation <- df2 %>%
+  mutate(gdp_capita_some_na = replace(gdp_capita_some_na,
+                                      is.na(gdp_capita_some_na),
+                                      mean(gdp_capita_some_na, na.rm = TRUE)))
+
+1#2. KNN method using Machine Learning?
